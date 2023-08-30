179fe899dd23920c0f3dd149a96bea15dc1a2f2d
WorkPC <paynedanielfranklin@gmail.com>
2023-08-30 00:08:42 -0400

    first commit

---
+++ colors.js
@@ -0,0 +1,42 @@
+  // Define ANSI color codes
+const colors = {
+    reset: "\x1b[0m",
+    black: "\x1b[30m",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    magenta: "\x1b[35m",
+    cyan: "\x1b[36m",
+    white: "\x1b[37m",
+    
+  };
+
+  const bg = {
+    black: "\x1b[40m",
+    red: "\x1b[41m",
+    green: "\x1b[42m",
+    yellow: "\x1b[43m",
+    blue: "\x1b[44m",
+    magenta: "\x1b[45m",
+    cyan: "\x1b[46m",
+    white: "\x1b[47m",
+  }
+  
+  // Define ANSI style codes
+  const styles = {
+    reset: "\x1b[0m",
+    bold: "\x1b[1m",
+    dim: "\x1b[2m",
+    italic: "\x1b[3m",
+    underline: "\x1b[4m",
+    inverse: "\x1b[7m",
+    hidden: "\x1b[8m",
+    strikethrough: "\x1b[9m",
+  };
+
+  module.exports = {
+    colors,
+    styles,
+    bg
+  }
---
+++ index.js
@@ -0,0 +1,42 @@
+const { colors, styles, bg } = require('./colors');
+
+class Logtastic {
+    constructor() {
+        this.colors = colors;
+        this.styles = styles;
+        this.bg = bg;
+    }
+
+
+    /**
+     * Logs a message with customizable color, style, and background.
+     *
+     * @param {string|object} text - The text or object to be logged.
+     * @param {Object} options - Options for styling the output.
+     *   @param {string} [options.color] - The text color to apply.
+     *     Possible values: 'reset', 'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'.
+     *   @param {string} [options.style] - The text style to apply.
+     *     Possible values: 'reset', 'bold', 'dim', 'italic', 'underline', 'inverse', 'hidden', 'strikethrough'.
+     *   @param {string} [options.bgStyle] - The background color to apply.
+     *     Possible values: 'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'.
+     * @returns {void}
+     */
+    log(text, { color = this.colors.reset, style = this.styles.reset, bgStyle }) {
+        const appliedColors = color ? this.colors[color] : "";
+        const appliedStyles = style ? this.styles[style] : "";
+        const appliedBg = bgStyle ? this.bg[bgStyle] : "";
+        try {
+            if (typeof text === 'string') {
+                console.log(`${appliedColors}${appliedStyles}${appliedBg}${text}${colors.reset}${styles.reset}`);
+            } else {
+                const styledObject = JSON.stringify(text, null, 2); // Pretty-print with 2-space indentation
+                console.log(`${appliedColors}${appliedStyles}${appliedBg}${styledObject}${styles.reset}${colors.reset}`);
+            }
+        } catch (err) {
+            console.warn('Issue with Logtastic. Which is not very logtastic of it');
+            console.log(text);
+        }
+    }
+}
+
+module.exports = Logtastic;
